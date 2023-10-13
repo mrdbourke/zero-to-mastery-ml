72026c9fb879f5853023adb03bfe96afe513a2dd
mrdbourke <danielbourke@icloud.com>
2023-10-13 10:31:35 +1000

    update docs

--- make_docs.sh
+++ make_docs.sh
@@ -5,6 +5,7 @@ cp section-2-data-science-and-ml-tools/introduction-to-numpy.ipynb docs/
 cp section-2-data-science-and-ml-tools/introduction-to-pandas.ipynb docs/
 cp section-2-data-science-and-ml-tools/introduction-to-matplotlib.ipynb docs/
 cp section-2-data-science-and-ml-tools/introduction-to-scikit-learn.ipynb docs/
+cp communicating-your-work.md docs/
 cp images/* docs/images/
 
 # Launch mkdocs server
