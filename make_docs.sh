e9326418658385c99cded707d5219ac3fc3299f1
mrdbourke <danielbourke@icloud.com>
2024-04-04 15:01:01 +1000

    update docs creation and dog vision notebook

--- make_docs.sh
+++ make_docs.sh
@@ -5,6 +5,7 @@ cp section-2-data-science-and-ml-tools/introduction-to-numpy.ipynb docs/
 cp section-2-data-science-and-ml-tools/introduction-to-pandas.ipynb docs/
 cp section-2-data-science-and-ml-tools/introduction-to-matplotlib.ipynb docs/
 cp section-2-data-science-and-ml-tools/introduction-to-scikit-learn.ipynb docs/
+cp section-4-unstructured-data-projects/end-to-end-dog-vision-v2.ipynb docs/
 cp communicating-your-work.md docs/
 cp images/* docs/images/
 
