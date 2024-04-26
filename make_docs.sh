490567c823972a88bf521e458a5882a9f3947dc6
mrdbourke <danielbourke@icloud.com>
2024-04-26 12:24:22 +1000

    update dog vision v2 notebook

--- make_docs.sh
+++ make_docs.sh
@@ -9,5 +9,8 @@ cp section-4-unstructured-data-projects/end-to-end-dog-vision-v2.ipynb docs/
 cp communicating-your-work.md docs/
 cp images/* docs/images/
 
+# Remove .ipynb_checkpoints from docs/
+rm -rf docs/.ipynb_checkpoints
+
 # Launch mkdocs server
 mkdocs serve
