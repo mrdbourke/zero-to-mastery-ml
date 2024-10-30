8b3c01d3a3208e4b82200a3975647fc9b59a4ce7
mrdbourke <danielbourke@icloud.com>
2024-10-30 13:00:55 +1000

    update docs creation

--- make_docs.sh
+++ make_docs.sh
@@ -6,6 +6,8 @@ cp section-2-data-science-and-ml-tools/introduction-to-pandas.ipynb docs/
 cp section-2-data-science-and-ml-tools/introduction-to-matplotlib.ipynb docs/
 cp section-2-data-science-and-ml-tools/introduction-to-scikit-learn.ipynb docs/
 cp section-4-unstructured-data-projects/end-to-end-dog-vision-v2.ipynb docs/
+cp section-3-structured-data-projects/end-to-end-heart-disease-classification.ipynb docs/ 
+cp section-3-structured-data-projects/end-to-end-bluebook-bulldozer-price-regression-v2.ipynb docs/
 cp communicating-your-work.md docs/
 cp images/* docs/images/
 
